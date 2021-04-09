325bd343da9ede21cff7f17ef0c05ff52cc29c43
pp <pp31837@gmail.com>
2021-04-09 14:28:01 +0700

    a

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
         var a = 0;
         var b = 0;
-        var _intervalId = 0;
+        var c = 0;
+        var d = 0;
+        var _intervalId01 = 0;
+        var _intervalId02 = 0;
+        var _intervalId03 = 0;
+        var _intervalId04 = 0;
 
-        function countup() {
-            // if (a < 10) {
-            //     document.getElementById("pp-count01").innerHTML = a;
-            //     a++;
-            // } else {
-            //     // a=0;
-            //     // clearInterval(_intervalId);
-            // }
-            document.getElementById("pp-count01").innerHTML = a;
-                a++;
+        function countup(countId,x) {
 
+            document.getElementById(countId).innerHTML = x;
+                x++;
         }
 
-        _intervalId = setInterval(countup,500);
+        _intervalId01 = setInterval(countup,1500,"pp-count01",a);
+        _intervalId02 = setInterval(countup,300,"pp-count02",b);
+        _intervalId03 = setInterval(countup,1000,"pp-count03",c);
+        _intervalId04 = setInterval(countup,1000,"pp-count04",d);
